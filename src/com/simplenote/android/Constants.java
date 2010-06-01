7b58473b7591765317b38c038a07b0716bb87448
Craig P Jolicoeur <cpjolicoeur@gmail.com>
2010-06-01 12:12:17 -0400

    add temp constants file

---
+++ src/com/simplenote/android/Constants.java
@@ -0,0 +1,27 @@
+package com.simplenote.android;
+
+
+public class Constants {
+	static final boolean LOGGING = true;
+	
+	static final boolean USE_CACHE = true;
+	
+    // User-defined result codes
+    
+    // Menu and dialog actions
+//    static final int DIALOG_LOGIN = 2;
+
+    // progress dialogs
+//    static final int DIALOG_LOGGING_IN = 1000;
+    
+    
+    // JSON values
+//    static final String JSON_AFTER = "after";
+    
+    
+    // Preference keys and values
+
+	// API Base URL
+	static final String API_BASE_URL = "http://simple-note.appspot.com/api";
+}
+

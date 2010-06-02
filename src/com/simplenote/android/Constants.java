372d693090a5a97695998885336199b0b2181e12
Craig P Jolicoeur <cpjolicoeur@gmail.com>
2010-06-01 22:15:39 -0400

    add rest of API route Constants

--- src/com/simplenote/android/Constants.java
+++ src/com/simplenote/android/Constants.java
@@ -24,7 +24,12 @@ public class Constants {
     // Preference keys and values
 
 	// API Base URL
-	static final String API_BASE_URL  = "http://simple-note.appspot.com/api";
-	static final String API_LOGIN_URL = "https://simple-note.appspot.com/api/login";
+	static final String API_BASE_URL   = "https://simple-note.appspot.com/api";
+	static final String API_LOGIN_URL  = API_BASE_URL + "/login";					// POST
+	static final String API_NOTES_URL  = API_BASE_URL + "/index";					// GET
+	static final String API_NOTE_URL   = API_BASE_URL + "/note";					// GET
+	static final String API_UPDATE_URL = API_BASE_URL + "/note"; 					// POST
+	static final String API_DELETE_URL = API_BASE_URL + "/delete";					// GET
+	static final String API_SEARCH_URL = API_BASE_URL + "/search";					// GET
 }
 

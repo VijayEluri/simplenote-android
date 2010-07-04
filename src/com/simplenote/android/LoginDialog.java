a7641fa19d1255db47e8b654ad96e872b958b3b1
Bryan J Swift <bryan.j.swift@gmail.com>
2010-07-03 23:02:01 -0400

    Remove button and make keyboard actions handle login

--- src/com/simplenote/android/LoginDialog.java
+++ src/com/simplenote/android/LoginDialog.java
@@ -8,12 +8,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.view.View.OnClickListener;
+import android.view.KeyEvent;
 import android.view.ViewGroup.LayoutParams;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 import android.widget.Toast;
 
 import com.simplenote.android.APIBase.Response;
@@ -49,10 +49,18 @@ public class LoginDialog extends Activity {
 
 		loginBox.setText(mPrefs.getString("email", ""));
 		passwordBox.setText(mPrefs.getString("password", ""));
-
-		Button loginBtn = (Button) findViewById(R.id.loginBtn);
-		loginBtn.setOnClickListener(mLoginButtonClick);
-
+		passwordBox.setOnEditorActionListener(new OnEditorActionListener() {
+			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+				mThread = new Thread(signinRunnable);
+				mProgressDialog = ProgressDialog.show(LoginDialog.this, "Logging in...", "Initializing...");
+				if (Constants.LOGGING) {
+					Log.i(Constants.TAG, "Starting login thread");
+				}
+				mThread.start();
+				return false;
+			}
+			
+		});
 	}
 
 	@Override
@@ -60,31 +68,31 @@ public class LoginDialog extends Activity {
 		if ( mProgressDialog != null && mProgressDialog.isShowing() ) { mProgressDialog.dismiss(); }
 		super.onPause();
 	}
-
-	private Runnable threadProcLogin = new Runnable() {
+	
+	private Runnable signinRunnable = new Runnable() {
 		public void run() {
 			EditText loginBox = (EditText) findViewById(R.id.email);
 			EditText passwordBox = (EditText) findViewById(R.id.password);
-
+	
 			String email = loginBox.getText().toString();
 			String password = passwordBox.getText().toString();
-
+	
 			runOnUiThread(new Runnable() {
 				public void run() {
 					mProgressDialog.setMessage(authenticating);
 					mProgressDialog.show();
 				}
 			});
-
+	
 			if (Constants.LOGGING) { 
 				Log.d(Constants.TAG, "Attempting login authentication with API server."); 
 				Log.d(Constants.TAG, "email: " + email + ", password: " + password); 
 			}
-
+	
 			String authBody = APIBase.encode( "email=" + email + "&password=" + password, true, true );
 			if ( Constants.LOGGING ) { Log.d(Constants.TAG, "encoded authBody: " + authBody); }
 			Response authResponse = APIBase.HTTPPost( Constants.API_LOGIN_URL, authBody );
-
+	
 			if ( authResponse.statusCode == 401 ) { // failed auth login
 				if ( Constants.LOGGING ) { Log.i(Constants.TAG, "Login auth failed with API server."); }
 				runOnUiThread( new Runnable() {
@@ -99,12 +107,12 @@ public class LoginDialog extends Activity {
 				mPrefsEditor.putString("password", password);
 				mPrefsEditor.putString("token", authResponse.resp);
 				mPrefsEditor.commit();
-
+	
 				// Refresh the notes when logging in. TODO: Make this happen in the background
 				String logInToken = authResponse.resp.replaceAll("(\\r|\\n)", "");
 				APIHelper apiHelper = new APIHelper();
 				apiHelper.clearAndRefreshNotes(getApplicationContext(), logInToken, email);
-
+	
 				runOnUiThread( new Runnable() {
 					public void run() {
 						mProgressDialog.dismiss();
@@ -114,17 +122,6 @@ public class LoginDialog extends Activity {
 					}
 				});
 			}
-		}
-	};
-
-	private OnClickListener mLoginButtonClick = new OnClickListener() {
-		public void onClick(View v) {
-			mThread = new Thread(threadProcLogin);
-			mProgressDialog = ProgressDialog.show(LoginDialog.this, "Logging in...", "Initializing...");
-			if (Constants.LOGGING) {
-				Log.i(Constants.TAG, "Starting login thread");
-			}
-			mThread.start();
 		}
 	};
 }

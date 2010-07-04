51c0d99812f220c11b9e6ce9da50b0308e069aa4
Bryan J Swift <bryan.j.swift@gmail.com>
2010-07-04 18:03:22 -0400

    Change date format
    Also convert spaces to tabs

--- src/com/simplenote/android/APIHelper.java
+++ src/com/simplenote/android/APIHelper.java
@@ -16,55 +16,55 @@ import android.util.Log;
 import com.simplenote.android.APIBase.Response;
 
 public class APIHelper {
-    private NotesDbAdapter mDbHelper;
+	private NotesDbAdapter mDbHelper;
    
 	public void clearAndRefreshNotes(Context context, String token, String email) {
-        mDbHelper = new NotesDbAdapter(context);
-        mDbHelper.open();
-	    mDbHelper.deleteAllNotes();
-        mDbHelper.close();
-        
-        refreshNotes(context, token, email);
+		mDbHelper = new NotesDbAdapter(context);
+		mDbHelper.open();
+		mDbHelper.deleteAllNotes();
+		mDbHelper.close();
+		
+		refreshNotes(context, token, email);
 	}
 	
 	public void refreshNotes(Context context, String token, String email) {
-        mDbHelper = new NotesDbAdapter(context);
+		mDbHelper = new NotesDbAdapter(context);
 
 		Response authResponse = APIBase.HTTPGet(Constants.API_NOTES_URL + "?auth=" + token + "&email=" + email);
 		
 		JSONArray jsonNotes;
 		try {
 			jsonNotes = new JSONArray(authResponse.resp);
-	        mDbHelper.open();
+			mDbHelper.open();
 
 			for (int i = 0; i < jsonNotes.length(); ++i) {
-			    JSONObject jsonNote = jsonNotes.getJSONObject(i);
-			    String key = jsonNote.getString("key");
-			    Date modify = parseDate(jsonNote.getString("modify"));
-			    
-			    if (! jsonNote.getString("deleted").equals("true")) {
-				    if (! (checkNoteExists(key) && ! mDbHelper.checkNewerNote(key, modify))) {		
-				    	if (mDbHelper.checkNewerNote(key, modify)) {
+				JSONObject jsonNote = jsonNotes.getJSONObject(i);
+				String key = jsonNote.getString("key");
+				Date modify = parseDate(jsonNote.getString("modify"));
+				
+				if (! jsonNote.getString("deleted").equals("true")) {
+					if (! (checkNoteExists(key) && ! mDbHelper.checkNewerNote(key, modify))) {		
+						if (mDbHelper.checkNewerNote(key, modify)) {
 							if ( Constants.LOGGING ) { Log.i(Constants.TAG, "Note " + key + " is newer on server - retrieving"); }
-				    		mDbHelper.deleteNote(key);
-				    	} else {
-				    		if ( Constants.LOGGING ) { Log.i(Constants.TAG, "Note " + key + " is missing from device - retrieving"); }
-				    	}
-					    
-					    authResponse = APIBase.HTTPGet(Constants.API_NOTE_URL + "?key=" + key + "&auth=" + token + "&email=" + email);
-					    String title = authResponse.resp;
-					    if (title.indexOf('\n') > -1) {
-					    	title = title.substring(0, title.indexOf('\n'));
-					    }
+							mDbHelper.deleteNote(key);
+						} else {
+							if ( Constants.LOGGING ) { Log.i(Constants.TAG, "Note " + key + " is missing from device - retrieving"); }
+						}
 						
-				        mDbHelper.createNote(key, title, authResponse.resp, jsonNote.getString("modify"));
-				    }
-			    }
+						authResponse = APIBase.HTTPGet(Constants.API_NOTE_URL + "?key=" + key + "&auth=" + token + "&email=" + email);
+						String title = authResponse.resp;
+						if (title.indexOf('\n') > -1) {
+							title = title.substring(0, title.indexOf('\n'));
+						}
+						
+						mDbHelper.createNote(key, title, authResponse.resp, jsonNote.getString("modify"));
+					}
+				}
 			}	
 		} catch (JSONException e) {
 			e.printStackTrace();
 		} finally {
-	        mDbHelper.close();
+			mDbHelper.close();
 		}
 	}
 
@@ -89,18 +89,18 @@ public class APIHelper {
 					+ "&auth=" + token, authBody);
 			
 			// Update the note key
-	        mDbHelper = new NotesDbAdapter(context);
-	        mDbHelper.open();
-	        mDbHelper.addKeyToNote(rowId, authResponse.resp.replaceAll("(\\r|\\n)", ""));
-	        mDbHelper.close();
+			mDbHelper = new NotesDbAdapter(context);
+			mDbHelper.open();
+			mDbHelper.addKeyToNote(rowId, authResponse.resp.replaceAll("(\\r|\\n)", ""));
+			mDbHelper.close();
 		}
 		
 		return false;
 	}
 	
 	public static Date parseDate(String date) {
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        try {
+		DateFormat dateFormat = new SimpleDateFormat("yyyy MMM dd");
+		try {
 			return dateFormat.parse(date);
 		} catch (ParseException e) {
 			e.printStackTrace();

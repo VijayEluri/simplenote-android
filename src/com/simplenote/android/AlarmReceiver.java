e87941375b3feeef1dc4f8b6028927d6659ec144
Bryan J Swift <bryan.j.swift@gmail.com>
2010-07-05 17:30:16 -0400

    Remove unused imports

--- src/com/simplenote/android/AlarmReceiver.java
+++ src/com/simplenote/android/AlarmReceiver.java
@@ -1,13 +1,12 @@
 package com.simplenote.android;
 
-import com.simplenote.android.APIBase.Response;
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
+
+import com.simplenote.android.APIBase.Response;
 
 public class AlarmReceiver extends BroadcastReceiver {
 	private static final String LOGGING_TAG = Constants.TAG + "AlarmReceiver";

e292e161332d0250ddf750334b691f8acb1a2962
Bryan J Swift <bryan.j.swift@gmail.com>
2010-07-05 16:06:02 -0400

    Update AlarmReceiver to use constants in Preferences

--- src/com/simplenote/android/AlarmReceiver.java
+++ src/com/simplenote/android/AlarmReceiver.java
@@ -10,7 +10,6 @@ import android.util.Log;
 import android.widget.Toast;
 
 public class AlarmReceiver extends BroadcastReceiver {
-
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		try {
@@ -18,16 +17,16 @@ public class AlarmReceiver extends BroadcastReceiver {
 
 			APIHelper apiHelper = new APIHelper();
 			Bundle bundle = intent.getExtras();
-			String email = bundle.getString("email");
-			String password = bundle.getString("password");
+			String email = bundle.getString(Preferences.EMAIL);
+			String password = bundle.getString(Preferences.PASSWORD);
 
-			String authBody = APIBase.encode( "email=" + email + "&password=" + password, true );
-			Response authResponse = APIBase.HTTPPost( Constants.API_LOGIN_URL, authBody );
+			String authBody = APIBase.encode("email=" + email + "&password=" + password, true);
+			Response authResponse = APIBase.HTTPPost(Constants.API_LOGIN_URL, authBody);
 
 			if (authResponse.statusCode == 200) { // successful auth login
-				if ( Constants.LOGGING ) { Log.i(Constants.TAG, "Login auth success with API server."); }
 				String logInToken = authResponse.resp.replaceAll("(\\r|\\n)", "");
 				apiHelper.refreshNotes(new NotesDbAdapter(context), logInToken, email);
+				Log.i(Constants.TAG, "Login auth success with API server.");
 			}
 		} catch (Exception e) {
 			Toast.makeText(context, "There was an error somewhere, but we still received an alarm", Toast.LENGTH_LONG).show();

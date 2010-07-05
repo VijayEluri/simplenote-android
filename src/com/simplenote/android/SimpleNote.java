c072a1b1d9372b5a10a62f44a0275d3b4c47d835
Bryan J Swift <bryan.j.swift@gmail.com>
2010-07-05 16:07:10 -0400

    Remove an uninformative comment

--- src/com/simplenote/android/SimpleNote.java
+++ src/com/simplenote/android/SimpleNote.java
@@ -48,7 +48,6 @@ public class SimpleNote extends ListActivity {
 		}
 	};
 
-	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);

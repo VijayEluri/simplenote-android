4cd3c4d3e9bbd4cb4a1b7d93225724b55b6b7d98
Bryan J Swift <bryan.j.swift@gmail.com>
2010-07-05 18:10:17 -0400

    Spacing OCD

--- test/com/simplenote/android/SimpleNoteTest.java
+++ test/com/simplenote/android/SimpleNoteTest.java
@@ -5,8 +5,8 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 public class SimpleNoteTest extends TestSuite {
-    public static Test suite() {
-        return new TestSuiteBuilder(SimpleNoteTest.class).includeAllPackagesUnderHere().build();
-    }
+	public static Test suite() {
+		return new TestSuiteBuilder(SimpleNoteTest.class).includeAllPackagesUnderHere().build();
+	}
 
 }

fa8f96110b9d6c8731df06a74803e973615fcd01
Bryan J Swift <bryan.j.swift@gmail.com>
2010-07-06 14:31:30 -0400

    Add some psuedo code to SimpleNote Activity

--- src/com/simplenote/android/ui/SimpleNote.java
+++ src/com/simplenote/android/ui/SimpleNote.java
@@ -1,8 +1,12 @@
 package com.simplenote.android.ui;
 
+import java.util.HashMap;
+
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.simplenote.android.R;
+import com.simplenote.android.Preferences;
 import com.simplenote.android.persistence.SimpleNoteDao;
 
 /**
@@ -10,7 +14,7 @@ import com.simplenote.android.persistence.SimpleNoteDao;
  * @author bryanjswift
  */
 public class SimpleNote extends Activity {
-	/** Database Adapter to simplify interactions with the Sqlite database */
+	/** Database Adapter to simplify interactions with the SQLite database */
 	private final SimpleNoteDao dao;
 	public SimpleNote() {
 		super();
@@ -22,6 +26,24 @@ public class SimpleNote extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		HashMap<String,String> credentials = Preferences.getLoginPreferences(this);
+		setContentView(R.layout.main);
+		if (credentials.containsKey(Preferences.EMAIL) && credentials.containsKey(Preferences.TOKEN)) { // valid token stored
+			// sync notes
+			// start note list activity
+		} else if (credentials.containsKey(Preferences.EMAIL) && credentials.containsKey(Preferences.PASSWORD)) { // token expired
+			// get new token from API
+			// when API successfully returns
+				// sync notes
+				// start note list activity
+		} else {
+			// set splash layout?
+			// should splash be the main Activity that starts SimpleNote if logged in?
+			// start login dialog for result
+			// when dialog successfully returns
+				// sync notes
+				// start note list activity
+		}
 	}
 	/**
 	 * @see android.app.Activity#onPause()

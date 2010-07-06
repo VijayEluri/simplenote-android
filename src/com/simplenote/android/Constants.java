6c831c82945bb026d4c3077c7aba86db5f59aa74
Bryan J Swift <bryan.j.swift@gmail.com>
2010-07-06 01:04:19 -0400

    Add some comments

--- src/com/simplenote/android/Constants.java
+++ src/com/simplenote/android/Constants.java
@@ -2,17 +2,17 @@ package com.simplenote.android;
 
 
 public class Constants {
+	/** Name of stored preferences */
 	public static final String PREFS_NAME = "SimpleNotePrefs";
+	/** Logging tag prefix */
 	public static final String TAG = "SimpleNote:";
-	public static final boolean USE_CACHE = true;
 
 	// API Base URL
 	public static final String API_BASE_URL   = "https://simple-note.appspot.com/api";
 	public static final String API_LOGIN_URL  = API_BASE_URL + "/login";					// POST
 	public static final String API_NOTES_URL  = API_BASE_URL + "/index";					// GET
-	public static final String API_NOTE_URL   = API_BASE_URL + "/note";					// GET
-	public static final String API_UPDATE_URL = API_BASE_URL + "/note"; 					// POST
+	public static final String API_NOTE_URL   = API_BASE_URL + "/note";						// GET
+	public static final String API_UPDATE_URL = API_BASE_URL + "/note";						// POST
 	public static final String API_DELETE_URL = API_BASE_URL + "/delete";					// GET
 	public static final String API_SEARCH_URL = API_BASE_URL + "/search";					// GET
 }
-

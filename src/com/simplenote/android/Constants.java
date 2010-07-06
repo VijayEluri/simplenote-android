25337e5f295c951cbe74c499d5964a951305c975
Bryan J Swift <bryan.j.swift@gmail.com>
2010-07-05 21:16:32 -0400

    Make API urls public constants

--- src/com/simplenote/android/Constants.java
+++ src/com/simplenote/android/Constants.java
@@ -7,12 +7,12 @@ public class Constants {
 	public static final boolean USE_CACHE = true;
 
 	// API Base URL
-	static final String API_BASE_URL   = "https://simple-note.appspot.com/api";
-	static final String API_LOGIN_URL  = API_BASE_URL + "/login";					// POST
-	static final String API_NOTES_URL  = API_BASE_URL + "/index";					// GET
-	static final String API_NOTE_URL   = API_BASE_URL + "/note";					// GET
-	static final String API_UPDATE_URL = API_BASE_URL + "/note"; 					// POST
-	static final String API_DELETE_URL = API_BASE_URL + "/delete";					// GET
-	static final String API_SEARCH_URL = API_BASE_URL + "/search";					// GET
+	public static final String API_BASE_URL   = "https://simple-note.appspot.com/api";
+	public static final String API_LOGIN_URL  = API_BASE_URL + "/login";					// POST
+	public static final String API_NOTES_URL  = API_BASE_URL + "/index";					// GET
+	public static final String API_NOTE_URL   = API_BASE_URL + "/note";					// GET
+	public static final String API_UPDATE_URL = API_BASE_URL + "/note"; 					// POST
+	public static final String API_DELETE_URL = API_BASE_URL + "/delete";					// GET
+	public static final String API_SEARCH_URL = API_BASE_URL + "/search";					// GET
 }
 

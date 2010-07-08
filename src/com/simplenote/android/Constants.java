6559c13d39dd908e7b79fde2e2299a8935707978
Bryan J Swift <bryan.j.swift@gmail.com>
2010-07-08 15:57:06 -0400

    Add constant for an edit note request

--- src/com/simplenote/android/Constants.java
+++ src/com/simplenote/android/Constants.java
@@ -10,6 +10,7 @@ public class Constants {
 	public static final int MESSAGE_UPDATE_NOTE = 12398;
 	// Activity for result request Codes
 	public static final int REQUEST_LOGIN = 32568;
+	public static final int REQUEST_EDIT = 9138171;
 	// API Base URL
 	public static final String API_BASE_URL   = "https://simple-note.appspot.com/api";
 	public static final String API_LOGIN_URL  = API_BASE_URL + "/login";					// POST

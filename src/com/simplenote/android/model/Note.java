d8fbddb3aac503e0966b83b176b1fc04c16a3f81
Bryan J Swift <bryan.j.swift@gmail.com>
2010-07-08 15:56:27 -0400

    Don't store body without title information

--- src/com/simplenote/android/model/Note.java
+++ src/com/simplenote/android/model/Note.java
@@ -19,7 +19,6 @@ public class Note implements Serializable{
 	private final String key;
 	private final String dateModified;
 	private final boolean deleted;
-	private int newlines = 0;
 
 	/**
 	 * Private constructor taking all fields
@@ -29,17 +28,14 @@ public class Note implements Serializable{
 	 * @param key from SimpleNote servers for this note
 	 * @param dateModified - the last modification was made on this date (server or local)
 	 * @param deleted - whether or not the Note is 'deleted'
-	 * @param newlines - number of new lines separating title and body
 	 */
-	private Note(final long id, final String title, final String body, final String key,
-			final String dateModified, final boolean deleted, final int newlines) {
+	public Note(final long id, final String title, final String body, final String key, final String dateModified, final boolean deleted) {
 		this.id = id;
 		this.title = title;
 		this.body = body;
 		this.key = key;
 		this.dateModified = dateModified;
 		this.deleted = deleted;
-		this.newlines = newlines;
 	}
 	/**
 	 * Create a Note from all four pieces of information
@@ -52,21 +48,7 @@ public class Note implements Serializable{
 	 * @param dateModified - the last modification was made on this date (server or local)
 	 */
 	public Note(final long id, final String title, final String body, final String key, final String dateModified) {
-		this(id, title, body, key, dateModified, false, 2);
-	}
-	/**
-	 * Create a Note from all four pieces of information
-	 *
-	 * Defaults to two new lines between title and body text
-	 * @param id of the note
-	 * @param title of the note (really just the first line of body)
-	 * @param body of the note
-	 * @param key from SimpleNote servers for this note
-	 * @param dateModified - the last modification was made on this date (server or local)
-	 * @param deleted - whether or not the Note is 'deleted'
-	 */
-	public Note(final long id, final String title, final String body, final String key, final String dateModified, final boolean deleted) {
-		this(id, title, body, key, dateModified, deleted, 2);
+		this(id, title, body, key, dateModified, false);
 	}
 	/**
 	 * Create a Note from body, key and dateModified
@@ -95,13 +77,6 @@ public class Note implements Serializable{
 		this.id = id;
 		if (idxNewline != -1) {
 			this.title = titleAndBody.substring(0,idxNewline);
-			titleAndBody = titleAndBody.substring(idxNewline);
-			idxNewline = titleAndBody.indexOf("\n");
-			while (idxNewline == 0) {
-				titleAndBody = titleAndBody.substring(1);
-				idxNewline = titleAndBody.indexOf("\n");
-				newlines++;
-			}
 			this.body = titleAndBody;
 		} else {
 			this.title = titleAndBody;
@@ -119,7 +94,7 @@ public class Note implements Serializable{
 	 * @param deleted - whether or not the note is marked for deletion on the server
 	 */
 	public Note(final long id, final String key, final String dateModified, final boolean deleted) {
-		this(id, null, null, key, dateModified, deleted, 0);
+		this(id, null, null, key, dateModified, deleted);
 	}
 	/**
 	 * Create a Note from a JSONObject
@@ -127,7 +102,7 @@ public class Note implements Serializable{
 	 * @throws JSONException if a mapping doesn't exist or a coercion can't be made
 	 */
 	public Note(final JSONObject object) throws JSONException {
-		this(-1, "", "", object.getString("key"), object.getString("modify"), object.getBoolean("deleted"), 0);
+		this(-1, "", "", object.getString("key"), object.getString("modify"), object.getBoolean("deleted"));
 	}
 	/**
 	 * @return id
@@ -141,7 +116,7 @@ public class Note implements Serializable{
 	 * @return a new Note with id updated
 	 */
 	public final Note setId(final long id) {
-		return new Note(id, this.title, this.body, this.key, this.dateModified, this.deleted, this.newlines);
+		return new Note(id, this.title, this.body, this.key, this.dateModified, this.deleted);
 	}
 	/**
 	 * @return title
@@ -155,7 +130,7 @@ public class Note implements Serializable{
 	 * @return a new Note with title updated
 	 */
 	public final Note setTitle(final String title) {
-		return new Note(this.id, title, this.body, this.key, this.dateModified, this.deleted, this.newlines);
+		return new Note(this.id, title, this.body, this.key, this.dateModified, this.deleted);
 	}
 	/**
 	 * @return body
@@ -169,7 +144,7 @@ public class Note implements Serializable{
 	 * @return a new Note with body updated
 	 */
 	public final Note setBody(final String body) {
-		return new Note(this.id, this.title, body, this.key, this.dateModified, this.deleted, this.newlines);
+		return new Note(this.id, this.title, body, this.key, this.dateModified, this.deleted);
 	}
 	/**
 	 * @return key
@@ -183,7 +158,7 @@ public class Note implements Serializable{
 	 * @return a new Note with key updated
 	 */
 	public final Note setKey(final String key) {
-		return new Note(this.id, this.title, this.body, key, this.dateModified, this.deleted, this.newlines);
+		return new Note(this.id, this.title, this.body, key, this.dateModified, this.deleted);
 	}
 	/**
 	 * @return dateModified
@@ -197,7 +172,7 @@ public class Note implements Serializable{
 	 * @return a new Note with dateModified updated
 	 */
 	public final Note setDateModified(final String dateModified) {
-		return new Note(this.id, this.title, this.body, this.key, dateModified, this.deleted, this.newlines);
+		return new Note(this.id, this.title, this.body, this.key, dateModified, this.deleted);
 	}
 	/**
 	 * @return deleted
@@ -211,19 +186,14 @@ public class Note implements Serializable{
 	 * @return a new Note with deleted updated
 	 */
 	public final Note setDeleted(final boolean deleted) {
-		return new Note(this.id, this.title, this.body, this.key, this.dateModified, deleted, this.newlines);
+		return new Note(this.id, this.title, this.body, this.key, this.dateModified, deleted);
 	}
 	/**
-	 * Combine title and body fields with the correct number of newlines
+	 * Combine title and body fields
 	 * @return combined title and body fields
 	 */
 	public final String getTitleAndBody() {
-		StringBuilder builder = new StringBuilder(title);
-		for (int i = 0; i < newlines; i++) {
-			builder.append("\n");
-		}
-		builder.append(body);
-		return builder.toString();
+		return getBody();
 	}
 	/**
 	 * Create a new Note with updated title and body fields

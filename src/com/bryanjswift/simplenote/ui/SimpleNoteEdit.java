fb9de63c16dadeafd7d8b4d06d27a94cd70c0440
Bryan J Swift <bryan.j.swift@gmail.com>
2010-07-31 23:41:50 -0400

    Show virtual keyboard if editing a new note
    [#67 state:resolved]

--- src/com/bryanjswift/simplenote/ui/SimpleNoteEdit.java
+++ src/com/bryanjswift/simplenote/ui/SimpleNoteEdit.java
@@ -14,6 +14,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -108,7 +109,18 @@ public class SimpleNoteEdit extends Activity {
             }
         });
 	}
-	/**
+
+    /**
+     * @see android.app.Activity#onPostResume()
+     */
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        if (mNoteId == Constants.DEFAULT_ID) {
+            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+        }
+    }
+    /**
 	 * @see android.app.Activity#onResume()
 	 */
 	@Override

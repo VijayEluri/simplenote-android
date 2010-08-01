8287a443b1c3488db0c3fc252c14034ac4952396
Bryan J Swift <bryan.j.swift@gmail.com>
2010-08-01 18:57:35 -0400

    Add UpdateNoteTask as an AsyncTask object
    Handles the pushing of notes to the SimpleNote servers

---
+++ src/com/bryanjswift/simplenote/thread/UpdateNoteTask.java
@@ -0,0 +1,43 @@
+package com.bryanjswift.simplenote.thread;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.bryanjswift.simplenote.Constants;
+import com.bryanjswift.simplenote.Preferences;
+import com.bryanjswift.simplenote.model.Note;
+import com.bryanjswift.simplenote.net.Api;
+import com.bryanjswift.simplenote.net.ServerCreateCallback;
+import com.bryanjswift.simplenote.net.ServerSaveCallback;
+import com.bryanjswift.simplenote.net.SimpleNoteApi;
+
+/**
+ * Asyncronously send the note to the server
+ * @author bryanjswift
+ */
+public class UpdateNoteTask extends AsyncTask<Note, Void, Void> {
+    private static final String LOGGING_TAG = Constants.TAG + UpdateNoteTask.class.getSimpleName();
+    private final Context context;
+    public UpdateNoteTask(Context context) {
+        this.context = context;
+    }
+    @Override
+    protected Void doInBackground(Note... notes) {
+        final Note note = notes[0];
+        // Note modified, list refreshed by onResume
+        // Send updated note to the server
+        final Api.Credentials credentials = Preferences.getLoginPreferences(context);
+        if (!note.getKey().equals(Constants.DEFAULT_KEY) && note.isDeleted()) {
+            Log.d(LOGGING_TAG, "Deleting note on the server");
+            SimpleNoteApi.delete(note, credentials, new ServerSaveCallback(context, note));
+        } else if (note.getKey().equals(Constants.DEFAULT_KEY)) {
+            Log.d(LOGGING_TAG, "Creating a new note on the server");
+            SimpleNoteApi.create(note, credentials, new ServerCreateCallback(context, note));
+        } else {
+            Log.d(LOGGING_TAG, String.format("Sending note '%s' to SimpleNoteApi", note.getKey()));
+            SimpleNoteApi.update(note, credentials, new ServerSaveCallback(context, note));
+        }
+        return null;
+    }
+}

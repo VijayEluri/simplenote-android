1356aedad510b3a669bc57f5b143090b012a0b86
Bryan J Swift <bryan.j.swift@gmail.com>
2010-08-07 15:52:33 -0400

    Define other View constructors for ScrollWrappableEditText
    [#86]

--- src/com/bryanjswift/simplenote/view/ScrollWrappableEditText.java
+++ src/com/bryanjswift/simplenote/view/ScrollWrappableEditText.java
@@ -2,6 +2,7 @@ package com.bryanjswift.simplenote.view;
 
 import android.content.Context;
 import android.graphics.Rect;
+import android.util.AttributeSet;
 import android.util.Log;
 import android.widget.EditText;
 import android.widget.ScrollView;
@@ -21,6 +22,23 @@ public class ScrollWrappableEditText extends EditText {
      */
     public ScrollWrappableEditText(Context context) {
         super(context);
+    }
+    /**
+     * Default constructor
+     * @param context to which this View is being added
+     * @param attrs used when creating the View
+     */
+    public ScrollWrappableEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+    /**
+     * Default constructor
+     * @param context to which this View is being added
+     * @param attrs used when creating the View
+     * @param defStyle id of style definition to add to attrs
+     */
+    public ScrollWrappableEditText(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
     }
     /**
      * Don't request this view move if it is wrapped in a ScrollView
